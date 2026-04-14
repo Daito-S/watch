d612f1b2bd725c068aa900fe3d7ebe28db82bad1
Daito-S <218743916+Daito-S@users.noreply.github.com.>
2026-04-14 23:11:45 +0900

    短針を追加

--- clock.py
+++ clock.py
@@ -7,36 +7,50 @@ from PySide6.QtCore import QTimer, Qt
 class Clock(QWidget):
     def __init__(self):
         super().__init__()
-        self.angle = 0
+        self.minutes_angle = 0
+        self.hour_angle = 0
         self.setMinimumSize(200, 200)
 
         timer = QTimer(self)
-        # timer.timeout.connect(self.update_angle)
         timer.start(100)
 
-    # 針を1分(6度)回す
-    def update_angle(self):
-        self.angle = (self.angle + 6) % 360
+    # 短針を1時間分(30度)回す
+    def update_hour_angle(self):
+        self.hour_angle = (self.hour_angle + 30) % 360
         self.update()
 
-    # 針を進める
+    # 長針を進める(1分で6度)
     def advance_minutes(self, minutes):
-        self.angle = (self.angle + minutes * 6) % 360
+        # 変更前の分を取得(短針の更新判定に使用)
+        last_minutes = self.minutes_angle // 6 % 60
+
+        self.minutes_angle = (self.minutes_angle + minutes * 6) % 360 # 更新
+
+        # 短針更新の判定
+        if last_minutes + minutes >= 60:
+            count = (last_minutes + minutes) // 60
+            for i in range(count):
+                self.update_hour_angle()
         self.update()
 
     # 描画(update()時に実行)
     def paintEvent(self, event):
+        # 時計の針を描画
+        # 時計の中心から針の先端までの座標を計算して線を描く
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
 
-        w, h = self.width(), self.height()
-        cx, cy = w // 2, h // 2
-        length = min(w, h) // 2 - 10
+        # 長針と短針の描画をfor文でまとめる
+        for angle, width in [(self.hour_angle, 6), (self.minutes_angle, 4)]:
+            # 時計の中心と針の先端の座標を計算(短針なら長さは短く、長針なら長く)
+            cx = self.width() / 2
+            cy = self.height() / 2
+            length = min(cx, cy) * (0.5 if width == 6 else 0.8)
 
-        rad = math.radians(self.angle)
-        x = cx + length * math.sin(rad)
-        y = cy - length * math.cos(rad)
+            rad = math.radians(angle)
+            x = cx + length * math.sin(rad)
+            y = cy - length * math.cos(rad)
 
-        pen = QPen(Qt.black, 4)
-        painter.setPen(pen)
-        painter.drawLine(cx, cy, x, y)
+            pen = QPen(Qt.black, width)
+            painter.setPen(pen)
+            painter.drawLine(cx, cy, x, y)
