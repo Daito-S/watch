1fcf1084d80dbf505ea0bfa99d2abd9692d7d77c
Daito-S <218743916+Daito-S@users.noreply.github.com.>
2026-04-15 14:45:27 +0900

    初期時間設定用のスピンボックスを追加

--- input_panel.py
+++ input_panel.py
@@ -4,9 +4,29 @@ from PySide6.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QSpinBox, QPushBu
 class Input_Panel(QWidget):
     def __init__(self, on_add):
         super().__init__()
+        layout = QHBoxLayout()
+        # ---初期時間---
+        # 初期時間設定のボタン
+        self.set_initial_minutes_button = QPushButton("初期時間設定")
+        self.set_initial_minutes_button.clicked.connect(
+            self.set_initial_minutes)
+        # 時間指定のスピンボックス(短針)
+        self.initial_hour_spin = QSpinBox()
+        self.initial_hour_spin.setRange(1, 12)
+        self.initial_hour_spin.setSuffix(" 時")
+        # 時間指定のスピンボックス(長針)
+        self.initial_min_spin = QSpinBox()
+        self.initial_min_spin.setRange(0, 59)
+        self.initial_min_spin.setSuffix(" 分")
+        # レイアウトに追加
+        layout.addWidget(self.set_initial_minutes_button)
+        layout.addWidget(self.initial_hour_spin)
+        layout.addWidget(self.initial_min_spin)
+
+
+        # ---予定とその時間の追加---
         self.on_add = on_add
 
-        layout = QHBoxLayout()
 
         self.name_edit = QLineEdit()
         self.name_edit.setPlaceholderText("予定名")
@@ -26,6 +46,12 @@ class Input_Panel(QWidget):
 
         self.setLayout(layout)
 
+    # 初期時間設定ボタンがクリックされたときの処理
+    def set_initial_minutes(self):
+        minutes = self.initial_minutes_spin.value()
+        self.on_add("初期時間", minutes)
+
+    # 追加ボタンがクリックされたときの処理
     def add_clicked(self):
         name = self.name_edit.text()
         minutes = self.time_spin.value()
